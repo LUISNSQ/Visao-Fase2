b41a1b84ef0d08557707a13a3efab3ffac221f78
LUISNSQ <luis.miguel.gomes.05@gmail.com>
2025-11-21 16:59:05 +0000

    Add files via upload

--- pong-python-master (1)/pong-python-master/controlo.py
+++ pong-python-master (1)/pong-python-master/controlo.py
@@ -9,16 +9,20 @@ class ControloVisao:
     def __init__(self, area_min=8000):
 
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        print(self.device)
+        print("A usar:", self.device)
 
-        self.model = YOLO("yolov8s.pt")
+        #yolov8n para ser mais rápido
+        self.model = YOLO("yolov8n.pt")
         self.model.to(self.device)
-        self.target_class = 67 # a classe 67 é a classe do telemovel para a lista Ultralitics yolov8
+        self.target_class = 67  # classe do telemóvel
 
         self.cap = cv2.VideoCapture(0)
         if not self.cap.isOpened():
             print("Erro ao abrir a câmara.")
             sys.exit(1)
+
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
         self.area_min = area_min
         self.before = 0
@@ -35,22 +39,38 @@ class ControloVisao:
 
         #Calculo dos FPS
         now = time.time()
-        fps = 1 / (now - self.before)
+        if self.before == 0:
+            fps = 0
+        else:
+            fps = 1 / (now - self.before)
         self.before = now
 
-        results = self.model.predict(frame,verbose=False,device=str(self.device) )[0]
+        results = self.model.predict(
+            frame,
+            verbose=False,
+            device=str(self.device),
+            classes=[self.target_class],
+            imgsz=480
+        )[0]
+
         box_definida = None
         area_encontrada = 0
         pos_norm = None
 
         for box in results.boxes:
-            classe = int(box.cls[0]) # verifica a classe do COCO que encontrou
+
+            classe = int(box.cls[0].item())
             if classe != self.target_class:
                 continue
 
-            x1, y1, x2, y2 = map(int, box.xyxy[0])
-            area = (x2 - x1) * (y2 - y1)
+            conf = float(box.conf[0].item())
+            if conf < 0.6:
+                continue
 
+            x1, y1, x2, y2 = box.xyxy[0].tolist()
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+
+            area = (x2 - x1) * (y2 - y1)
             if area > self.area_min and area > area_encontrada:
                 area_encontrada = area
                 box_definida = (x1, y1, x2, y2)
@@ -60,18 +80,18 @@ class ControloVisao:
         if box_definida is not None:
             x1, y1, x2, y2 = box_definida
 
-            # borda
+            #retangulo
             cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
-            # centro
+            #centro
             cx = int((x1 + x2) / 2)
             cy = int((y1 + y2) / 2)
-            cv2.circle(img, (cx, cy), 5, (255, 0, 0), -1)
+            cv2.circle(img, (cx, cy), 5, (0, 255, 255), -1)
 
             pos_norm = cx / w
 
-        cv2.putText(img, f"FPS: {fps:.1f}", (10, 30),
-                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        cv2.putText(img, f"{fps:.1f} FPS", (10, 30),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
         cv2.imshow("Camera", img)
         cv2.waitKey(1)
